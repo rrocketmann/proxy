2c60ad235274192644eaeb55ae003b3329128ec3
rrocketmann <martinmalyshau@gmail.com>
2026-05-15 19:37:50 -0700

    Fix Vercel public folder

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import Server from 'bare-server-node';
 import nodeStatic from 'node-static';
 
 const bare = new Server('/bare/', '');
-const serve = new nodeStatic.Server('Site/');
+const serve = new nodeStatic.Server('public/');
 
 export default function handler(req, res) {
     if (bare.route_request(req, res)) return;
